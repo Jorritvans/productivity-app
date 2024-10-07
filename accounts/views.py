6bcb3702f03e1faefdcc5cef9717482f5f8bbb30
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-10-07 08:46:39 +0000

    Added UserRegistrationView for user registration endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+# View to handle user registration
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]  # Allow anyone to access this view

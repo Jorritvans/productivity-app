fe9fb83065f93b9f2e26a3a212695471cb39e535
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-10-07 08:40:20 +0000

    Added accounts app to INSTALLED_APPS

--- productivity_app/settings.py
+++ productivity_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'rest_framework',  # Django REST framework
     'corsheaders',     # CORS headers
     'cloudinary',      # Cloudinary

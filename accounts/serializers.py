0f2fb8dcb1f37fcef8ede5e5f49f3acea652943e
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-10-07 08:45:58 +0000

    Created UserRegistrationSerializer for account creation

---
+++ accounts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+# Serializer for user registration
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+    # Create user with encrypted password
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', '')
+        )
+        return user

d6b6e6a2cb0bd43d616dd989e26f450cd2915b5e
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-11-02 14:25:28 +0000

    fixing whitenoise issue in deployed version

--- productivity_app/settings.py
+++ productivity_app/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {

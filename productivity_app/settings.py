cb106749d57548a1c36d767446d94c72d8579c35
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-11-02 14:16:17 +0000

    setting debug to False in the correct way

--- productivity_app/settings.py
+++ productivity_app/settings.py
@@ -9,7 +9,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',

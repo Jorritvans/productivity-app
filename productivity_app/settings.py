803219d5c188c06f592299018f11b03f8a0d1df1
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-11-03 08:00:46 +0000

    keeping static root the same

--- productivity_app/settings.py
+++ productivity_app/settings.py
@@ -145,8 +145,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {

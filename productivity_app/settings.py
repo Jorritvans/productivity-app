3525d78825ecfdcc8858dc75946af667df39173b
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-11-03 07:57:31 +0000

    testing staticfiles

--- productivity_app/settings.py
+++ productivity_app/settings.py
@@ -145,7 +145,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {
